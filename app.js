15f31ede2da19c8cc89ae2cda6f74a3d26dcde3d
profdiegocarvalho <127697684+profdiegocarvalho@users.noreply.github.com>
2024-05-17 21:52:54 -0300

    Create app.js
    Criação do arquivo app.js que verifica se um número é par ou ímpar

---
+++ app.js
@@ -0,0 +1,11 @@
+// Definindo uma lista de números
+let numeros = [2,4,6,8,10]
+// Percorrendo a lista com um loop for
+for(let i = 0; i < numeros.length; i++){
+  // Verificando se o número é par ou ímpar
+  if(numero[i] % 2 === 0){
+    console.log(numero[i] + " é um número par);
+  }else{
+    console.log(numero[i] + " é um número ímpar);
+  }
+}
